221c5e53cbf106c16575bfaa24deeb11264a1e7a
Leandro Venâncio <leandrohilariovenancio@gmail.com>
2020-04-26 11:37:28 -0300

    Adicionado rota para criar transações e efetuado ajuste na rota listar transações

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
 const transactionsRepository = new TransactionsRepository();
 
+/**
+ * [x] GET /transactions: Essa rota deve retornar uma listagem com todas as transações
+ *                          que você cadastrou até agora
+ */
 transactionRouter.get('/', (request, response) => {
   try {
     const transactions = transactionsRepository.all();
@@ -17,9 +21,35 @@ transactionRouter.get('/', (request, response) => {
   }
 });
 
+/**
+ * [x] POST /transactions: A rota deve receber "title", "value" e "type" dentro do corpo da requisição,
+ *                          sendo "type" o tipo da transação, que deve ser
+ *                          "income" para entradas (depósitos) e
+ *                          "outcome" para saidas (retiradas).
+ * [x] Ao cadastrar uma nova transação, ela deve ser armazenada dentro de um objeto
+ *                          com o formato como o seguinte:
+ *                          {
+                              "id": "uuid",
+                              "title": "Salário",
+                              "value": 3000,
+                              "type": "income"
+                            }
+ */
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
